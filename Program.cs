92387bf834e5cb9acd1cac43f4ffd0518495d63e
Your Name <you@example.com>
2022-08-10 14:03:31 +0530

    [Megha] Add: Added UC3

--- Program.cs
+++ Program.cs
@@ -4,48 +4,51 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To OOPS Problems");
+            //Console.WriteLine("Welcome To OOPS Problems");
 
-            FetchInventoryDetails fetchInventoryDetails = new FetchInventoryDetails();
+            //FetchInventoryDetails fetchInventoryDetails = new FetchInventoryDetails();
 
 
             string filepath = "D:\\BridgelabzPracticeProblems\\OopsProblem\\OopsProblem\\InventoryManagement\\InventoryManagement\\Data.json";
 
-            Rice data = fetchInventoryDetails.Read(filepath);
+            //Rice data = fetchInventoryDetails.Read(filepath);
 
-            Console.WriteLine("Types of Rice :");
-            for (int i = 0; i < data.typesofRice.Count; i++)
-            {
+            //Console.WriteLine("Types of Rice :");
+            //for (int i = 0; i < data.typesofRice.Count; i++)
+            //{
 
-                Console.WriteLine(data.typesofRice[i].name);
-                Console.WriteLine(data.typesofRice[i].price);
-                Console.WriteLine(data.typesofRice[i].weight);
+            //    Console.WriteLine(data.typesofRice[i].name);
+            //    Console.WriteLine(data.typesofRice[i].price);
+            //    Console.WriteLine(data.typesofRice[i].weight);
 
-                Console.WriteLine("--------------");
-            }
+            //    Console.WriteLine("--------------");
+            //}
 
-            Console.WriteLine("Types of Pulses :");
-            for (int i = 0; i < data.typesofPulses.Count; i++)
-            {
+            //Console.WriteLine("Types of Pulses :");
+            //for (int i = 0; i < data.typesofPulses.Count; i++)
+            //{
 
-                Console.WriteLine(data.typesofPulses[i].name);
-                Console.WriteLine(data.typesofPulses[i].price);
-                Console.WriteLine(data.typesofPulses[i].weight);
+            //    Console.WriteLine(data.typesofPulses[i].name);
+            //    Console.WriteLine(data.typesofPulses[i].price);
+            //    Console.WriteLine(data.typesofPulses[i].weight);
 
-                Console.WriteLine("--------------");
-            }
+            //    Console.WriteLine("--------------");
+            //}
 
-            Console.WriteLine("Types of Wheat :");
-            for (int i = 0; i < data.typesofPulses.Count; i++)
-            {
+            //Console.WriteLine("Types of Wheat :");
+            //for (int i = 0; i < data.typesofPulses.Count; i++)
+            //{
 
-                Console.WriteLine(data.typesofWheat[i].name);
-                Console.WriteLine(data.typesofWheat[i].price);
-                Console.WriteLine(data.typesofWheat[i].weight);
+            //    Console.WriteLine(data.typesofWheat[i].name);
+            //    Console.WriteLine(data.typesofWheat[i].price);
+            //    Console.WriteLine(data.typesofWheat[i].weight);
 
-                Console.WriteLine("--------------");
-            }
-
+            //    Console.WriteLine("--------------");
+                //uc3
+                StockPortFolio port = new StockPortFolio();
+                port.DisplayStockDetails(filepath);
         }
+
+        
     }
 }
